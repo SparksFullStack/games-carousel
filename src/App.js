0d9bed7ffa6312e4d296ae6d1faaf67a988c49af
SparksFullStack <csparks@antioch.edu>
2018-09-05 18:55:34 -0700

    created general project structure and added reactstrap

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Card } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Card>
+          <p>test</p>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default App;
